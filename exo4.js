1cb9bef50a7573927f19c96e554a4372cc175de3
Elodie <elodiedecola@ymail.com>
2020-07-31 16:50:35 +0200

    exo4

---
+++ exo4.js
@@ -0,0 +1,5 @@
+// 4. Créez une constante que vous appellerez "CONSTANTE" et à laquelle vous assignerez la valeur "Je suis une constante".
+
+const CONSTANTE = "Je suis une constante";
+
+console.log(CONSTANTE);
