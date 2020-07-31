e52f94004a1b6b35f9e5a2e409f95e990beb9930
Elodie <elodiedecola@ymail.com>
2020-07-31 17:07:23 +0200

    exo5

---
+++ exo5.js
@@ -0,0 +1,4 @@
+// 5. Créez un tableau que vous appellerez "my_array" qui contiendra 6 éléments qui seront de type (dans cet ordre) "string", "integer", "string", "float", "string" et "bool" et qui auront respectivement pour valeur : "aux", "42", "Gloire", "42.42", "Pangolins" et "true".
+
+var my_array = ["aux", 42, "Gloire", 42.42, "Pangolins", true];
+
