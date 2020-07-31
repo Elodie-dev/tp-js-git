f1c758e22c300ad5f92fd2e07147b8cf0a4bbca8
Elodie <elodiedecola@ymail.com>
2020-07-31 17:15:52 +0200

    exo7

---
+++ exo7.js
@@ -0,0 +1,9 @@
+// 7. Créez une fonction que vous appellerez "my_concat" qui prend deux paramètres. La fonction devra afficher le premier paramètre suivi d’un espace suivi du second paramètre.
+
+function my_concat(a, b){
+
+    console.log(a + " " + b);
+    
+}
+
+my_concat("Hello", "World");
