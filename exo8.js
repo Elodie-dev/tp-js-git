5a24d7ccc5e96e392a65fdfd4684cbbd67b0afeb
Elodie <elodiedecola@ymail.com>
2020-07-31 17:27:56 +0200

    exo8

---
+++ exo8.js
@@ -0,0 +1,8 @@
+// 8. Créez une fonction "print_variable". Cette fonction devra afficher la chaîne de caractères suivante : "variable = [val]" où "[val]" est remplacé par la valeur de la variable passée en paramètre. 
+
+function print_variable(a) {
+    var variable = a;
+    console.log(variable);
+}
+
+print_variable("Hello");
