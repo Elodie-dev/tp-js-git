7e0b485849a7c50604a37d977575a59dbf1488f5
Elodie <elodiedecola@ymail.com>
2020-07-31 16:35:55 +0200

    exo2

---
+++ exo2.js
@@ -0,0 +1,5 @@
+// 2. Créez une variable "helpers" à laquelle vous assignerez la valeur "Padawan" suivi d’un retour à la ligne, et affichez-la dans la console.
+
+var helpers = "Padawan\n";
+
+console.log(helpers);
