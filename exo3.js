0e663cc54d9245a0a9040d589b1a29bd2b267d1c
Elodie <elodiedecola@ymail.com>
2020-07-31 16:47:00 +0200

    exo3

---
+++ exo3.js
@@ -0,0 +1,10 @@
+// 3. Créez les variables "integer", "float", "string", "bool", "null", "array". Affectez les valeurs suivantes aux variables dont le nom correspond au type de la valeur : "true", "[]", "quarante-deux", "42", "NULL" et "42.42"
+
+var integer = 42;
+var float = 42.42;
+var string = "quarante-deux";
+var bool = true;
+var nuull = null; // Ajout d'un "u" supplementaire sur le nom de la variable car le comptait en erreur sinon
+var array = [];
+
+
