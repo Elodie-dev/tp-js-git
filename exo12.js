af9d642d56adee8d525252013eafcafabc61c4b7
Elodie <elodiedecola@ymail.com>
2020-08-01 16:27:22 +0200

    exo12.js

---
+++ exo12.js
@@ -0,0 +1,13 @@
+// 12. Créez une fonction "array_key" qui devra retourner la valeur de l’élément du tableau situé à l’index "key".
+
+
+function array_key(key) {
+    array = ["Hello", "World", "!"];
+
+    console.log(array[key]);
+}
+
+
+array_key(0);
+array_key(1);
+array_key(2);
