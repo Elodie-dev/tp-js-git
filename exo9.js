3d5250f86a7b9207ed77c507901cc93d7192a489
Elodie <elodiedecola@ymail.com>
2020-08-02 15:35:13 +0200

    exo9

---
+++ exo9.js
@@ -0,0 +1,18 @@
+// 9. Créez une fonction "print_calls" qui ne prend aucun paramètre et qui affiche le nombre de fois qu’elle est appelée
+
+calls =0;
+
+function print_calls() {
+
+    calls++;
+
+    console.log(calls);
+
+}
+
+print_calls();
+print_calls();
+print_calls();
+print_calls();
+print_calls();
+print_calls();
